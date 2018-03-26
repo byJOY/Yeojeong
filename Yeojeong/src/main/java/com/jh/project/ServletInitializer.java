5c6a84c0615ee13e9f19b6921c7793f9aaaef129
DESKTOP-36Q695V\workj <work.jhlee@gmail.com>
2018-03-26 14:24:40 +0900

    s

--- Yeojeong/src/main/java/com/jh/project/ServletInitializer.java
+++ Yeojeong/src/main/java/com/jh/project/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-//안녕
+//안녕d
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(YeojeongApplication.class);
